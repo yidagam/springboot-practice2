047c8b494882dc887d05136fab1db917d2e4dab2
yidagam <yidagam@gmail.com>
2026-05-26 15:44:53 +0900

    upload files (first)

---
+++ src/main/java/com/springboot/prac/PracApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.prac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracApplication.class, args);
+	}
+
+}
